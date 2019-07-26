372374627d0df583fc3e6001e271fc87ee021322
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-26 23:10:15 +0000

    Automatically backed up by Learn

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -1,11 +1,4 @@
 require "nokogiri"
-require 'pry'
-
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  binding.pry
-end
 
 describe "#create_project_hash" do
 
